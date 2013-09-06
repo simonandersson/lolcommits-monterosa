59242676d307c6886bbb9c3bb54654dfcb8e98da
Simon Andersson <simon.andersson@monterosa.se>
2013-09-06 13:40:39 +0200

    set date

--- lib/lolcommits/runner.rb
+++ lib/lolcommits/runner.rb
@@ -2,7 +2,7 @@ module Lolcommits
   PLUGINS = Lolcommits::Plugin.subclasses
 
   class Runner
-    attr_accessor :capture_delay, :capture_device, :message, :sha,
+    attr_accessor :capture_delay, :capture_device, :date, :message, :sha,
       :snapshot_loc, :main_image, :repo, :config, :repo_internal_path,
       :font
 
@@ -28,6 +28,7 @@ module Lolcommits
       if self.sha.nil? || self.message.nil?
         git_info = GitInfo.new
         self.sha = git_info.sha if self.sha.nil?
+        self.date = git_info.date if self.date.nil?
         self.message = git_info.message if self.message.nil?
         self.repo_internal_path = git_info.repo_internal_path
         self.repo = git_info.repo
